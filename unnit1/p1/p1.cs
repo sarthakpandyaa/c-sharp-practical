432a7243a95bf4ba864f2ba8c1deaa88b654f0db
sarthakpandyaa <sarthak.pandya132215@marwadiuniversity.ac.in>
2026-02-17 07:50:19 +0530

    finding prime number between two values

--- unnit1/p1/p1.cs
+++ unnit1/p1/p1.cs
@@ -4,7 +4,32 @@ class Program
 {
   static void Main()
   {
-    string name = "John";
-    Console.WriteLine(name);
+    Console.Write("Enter a number: ");
+    int number = int.Parse(Console.ReadLine());
+
+    if (IsPrime(number))
+      Console.WriteLine($"{number} is a prime number.");
+    else
+      Console.WriteLine($"{number} is not a prime number.");
+  }
+
+  static bool IsPrime(int num)
+  {
+    if (num < 2)
+      return false;
+
+    if (num == 2)
+      return true;
+
+    if (num % 2 == 0)
+      return false;
+
+    for (int i = 3; i <= Math.Sqrt(num); i += 2)
+    {
+      if (num % i == 0)
+        return false;
+    }
+
+    return true;
   }
 }
